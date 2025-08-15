9670608b1cd23c5e71ece45605984024a15b8bc3
Gabriel Jones <jones.gabriel.a4@gmail.com>
2025-08-15 18:55:10 -0400

    Configuracion inicial 3.0

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('API Appstore')
+    .setDescription(
+      'Esta gestiona aplicaciones y permite a un usuario descargar aplicaciones',
+    )
+    .setVersion('3.0')
+    .addTag('app-store-api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-swagger', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
